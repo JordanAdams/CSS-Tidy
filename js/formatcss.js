b455c85f56e471d44308585c3d4c1dba630db3b8
Jordan Adams <jordan_callum@live.co.uk>
2012-03-07 08:15:12 +0000

    Updated Design

--- js/formatcss.js
+++ js/formatcss.js
@@ -1,7 +1,41 @@
-function $ (selector) {
-	return bonzo(qwery(selector));
+function minify_css(css) {
+
+	patt = /\/\*[\w\W]*?\*\//g;
+	css = css.replace(patt, '');
+
+	patt = /\s*{\s*/g;
+	css = css.replace(patt, '{');
+
+	patt = patt = /\s*}\s*/g;
+	css = css.replace(patt, '}');
+
+	patt = /;\s*/g;
+	css = css.replace(patt, ';');
+
+	return css;
+
 }
 
-var input = $('#input textarea').text();
+function format_css(css) {
 
-console.log(input);
+	css = minify_css(css);
+
+	/* prop:val */
+	pattern = /([^\{\}:;\s]+)\s*:\s*([^\s:;{}][^:;{}]+[^\s:;{}]|[^\s:;{}]+)\s*([;}])/g;
+	css = css.replace(pattern, '  $1: $2$3');
+
+	/* Opening { */
+	css = css.replace(/\{/g, ' {\n');
+
+	/* Ending ; */
+	css = css.replace(/;/g, ';\n');
+
+	/* Closing } */
+	css = css.replace(/\s*}/g, '\n}\n\n');
+
+	/* Whitepace at beginning */
+	css = css.replace(/\s*(.)/, '$1');
+
+	return css;
+
+}
